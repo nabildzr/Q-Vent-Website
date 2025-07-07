f139076de775872a5fdb4d76c15b86a94f491f22
Nanazz25 <mnazrielalamsyah25@gmail.com>
2025-07-07 21:05:33 +0700

    Add Component Sidebar, Sidebar, Header, Navbar, Script, And use the component in layout

---
+++ resources/views/components/heads.blade.php
@@ -0,0 +1,13 @@
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>{{$title ?? 'Gamma'}}</title>
+      <link rel="stylesheet" href="./assets/css/slick.css" />
+      <link rel="stylesheet" href="./assets/css/aos.css" />
+      <link rel="stylesheet" href="./assets/css/output.css" />
+      <link rel="stylesheet" href="./assets/css/style.css" />
+
+      {{ $head }}
+  </head>
