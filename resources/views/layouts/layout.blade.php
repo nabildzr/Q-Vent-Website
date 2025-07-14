3c4a8bab57dfd4e9e5fe4a76de0e7999e9e4ac34
nabildzr <muhammadnbillz@gmail.com>
2025-07-14 15:25:21 +0700

    Enhance layout structure: add title and head slots to head component, and script slot to script component

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,7 +1,14 @@
 <!-- meta tags and other links -->
 <!DOCTYPE html>
 <html lang="en" data-theme="light">
-  <x-head />
+  <x-head >
+    <x-slot:title>
+      @yield('title', 'Dashboard')
+    </x-slot:title>
+    <x-slot:head>
+      @yield('head')
+    </x-slot:head>
+  </x-head>
 
   <body>
     <x-sidebar />
@@ -14,6 +21,10 @@
       <x-footer />
     </main>
 
-    <x-script/>
+    <x-script> 
+      <x-slot:script>
+        @yield('scripts')
+      </x-slot:script>
+    </x-script>
   </body>
 </html>
