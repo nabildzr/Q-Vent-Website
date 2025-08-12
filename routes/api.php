aecfaf61fdafa2af2b347269e06b29d951980d55
nabildzr <muhammadnbillz@gmail.com>
2025-08-12 12:12:56 +0700

    Refactor API routes to improve structure and organization, adding user event retrieval endpoints.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\api\AuthenticationController;
+use App\Http\Controllers\api\EventController;
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,28 @@ Route::prefix('user')->group(function () {
     Route::post('sign-in', [AuthenticationController::class, 'signIn'])->name('signin');
     Route::post('send-code', [AuthenticationController::class, 'sendCode'])->name('send.code');
     Route::post('verify-code', [AuthenticationController::class, 'verifyCode'])->name('verify.code');
-    Route::middleware(['auth:sanctum'])->group(function () {
+    Route::put('recovery-password', [AuthenticationController::class, 'recoveryPassword']);
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // event (one)
+    Route::prefix('event')->group(function () {
+        Route::get('{id}', [EventController::class, 'getEventById']);
+    });
+
+    // events (many)
+    Route::prefix('events')->group(function () {
+        Route::get('/', [EventController::class, 'getAllEvents']);
+        
+    });
+
+    // user
+    Route::prefix('user')->group(function () {
         Route::get('sign-out', [AuthenticationController::class, 'signOut'])->name('signout');
+        Route::get('{id}/events', [EventController::class, 'getUserEvents']);
+        Route::get('{id}/events/upcoming', [EventController::class, 'getUserEventsUpcoming']);
+        Route::get('{id}/events/done', [EventController::class, 'getUserEventsDone']);
+        Route::get('{id}/events/ongoing', [EventController::class, 'getUserEventsOnGoing']);
+        Route::get('{id}', [UserController::class, 'getUserById']);
     });
 });
